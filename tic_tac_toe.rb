9d59b0fbffd356467e96d96527af4d2182be6f67
Max Timkovich <mtimkovich@gmail.com>
2011-09-03 17:57:16 -0500

    Board is now an object

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,10 +1,30 @@
 #!/usr/bin/ruby
 
-board = Array.new(3, Array.new(3))
+class Board
+  def initialize
+    @board = Array.new(3) { Array.new(3) }
+  end
 
-(0...board.length).each do |i|
-  (0...board[i].length).each do |j|
-    puts "(#{i},#{j})"
+  def draw_board
+    (0...@board.length).each do |y|
+      (0...@board[y].length).each do |x|
+        if @board[y][x].nil?
+          print ' '
+        elsif @board[y][x] == 1
+          print 'x'
+        else @board[y][x] == 2
+          print 'o'
+        end
+
+        if x == 2
+          puts
+        else
+          print '|'
+        end
+      end
+    end
   end
 end
 
+board = Board.new
+board.draw_board
