86bcc0ee91758f5db1ee57ed11b0cd218f576e66
Max Timkovich <mtimkovich@gmail.com>
2011-09-03 21:04:29 -0500

    Created set_cell method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,13 +2,13 @@
 
 class Board
   def initialize
-    @board = Array.new(3) { Array.new(3) }
+    @board = Array.new(3) { Array.new(3) {0} }
   end
 
   def draw_board
     (0...@board.length).each do |y|
       (0...@board[y].length).each do |x|
-        if @board[y][x].nil?
+        if @board[y][x] == 0
           print ' '
         elsif @board[y][x] == 1
           print 'x'
@@ -24,7 +24,15 @@ class Board
       end
     end
   end
+
+  def set_cell(x, y, value)
+    @board[y][x] = value
+  end
 end
 
 board = Board.new
+board.draw_board
+puts
+
+board.set_cell(1, 1, 1)
 board.draw_board
